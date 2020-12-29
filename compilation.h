07f070fc3552725775c806786974998744010bf1
jihene75 <62608713+jihene75@users.noreply.github.com>
2020-12-29 15:34:23 +0100

    Update compilation.h

--- compilation.h
+++ compilation.h
@@ -8,7 +8,13 @@ struct automate_fini_non_deterministe{
 	int s;	// etat initial
 	int* F; // etats accepteurs
 	int accept_F; // taille F
-	struct transitions** delta; //liste transitions 
+	struct transitions** delta; //ensemble des transitions 
 };
 
-
+struct automate_fini_deterministe {
+	int Q ; // nombre d'etats
+	int ** delta; // ensemble de transitions
+	int s; // etat initial
+	int* F; // etats accepteurs
+	int accept_F; //taille F
+};
